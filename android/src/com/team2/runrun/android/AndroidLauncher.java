1666cb0eaa70d7361518570f60f8254ae92ae232
Rueian Oneecom <rueiancsie@gmail.com>
2015-05-28 20:59:05 +0800

    init commit

---
+++ android/src/com/team2/runrun/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.team2.runrun.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.team2.runrun.RunRunGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new RunRunGame(), config);
+	}
+}
